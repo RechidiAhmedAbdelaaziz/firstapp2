bfee2dfce76639149da68d794b49dcd20ad4d27a
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2023-02-07 21:19:58 +0100

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -15,10 +16,34 @@ class MyApp extends StatelessWidget {
 }
 
 class FbApp extends StatelessWidget {
-  const FbApp({super.key});
+  const FbApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Facebook",
+          style:
+              TextStyle(color: Colors.blue[700], fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu, color: Colors.blue[700]),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search, color: Colors.blue[700]),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.message, color: Colors.blue[700],),
+          ),
+        ],
+      ),
+    );
   }
 }
