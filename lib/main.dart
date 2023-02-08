29ba8db04b715568b732929ab7f5d16a28f21124
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2023-02-08 08:49:35 +0100

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,34 +21,48 @@ class FbApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "facebook",
-          style:
-              TextStyle(color: Colors.blue[900], fontWeight: FontWeight.w700),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            "facebook",
+            style:
+                TextStyle(color: Colors.blue[900], fontWeight: FontWeight.w700),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu, color: Colors.blue[900]),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.message, color: Colors.blue[900]),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search, color: Colors.blue[900]),
+            ),
+          ],
+          elevation: 10,
         ),
-        centerTitle: true,
-        leading: IconButton(
+        floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          icon: Icon(Icons.menu, color: Colors.blue[900]),
+          child: Icon(Icons.add),
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.message, color: Colors.blue[900]),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search, color: Colors.blue[900]),
-          ),
-        ],
-        elevation: 10,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Container(
+          child: Text("Ahmed Rechidi welcomed you ",style: TextStyle(fontWeight: FontWeight.w900),textAlign: TextAlign.center,),
+          margin: EdgeInsets.symmetric(vertical: 220,horizontal: 120),
+          padding: EdgeInsets.all(9),
+          height: 300,width: 500,
+          alignment: Alignment.center,
+          //transform: Matrix4.rotationZ(2),
+          decoration: BoxDecoration(
+            color: Colors.cyan,
+            border: Border.all(color: Colors.black, width: 2),
+            //borderRadius: BorderRadius.circular(20),
+            shape: BoxShape.circle,
+            ),
+
+        ));
   }
 }
